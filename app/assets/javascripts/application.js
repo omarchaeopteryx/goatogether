a68d62d32b97a54cbf2998de91c3e28c5bce78ae
Christine Duane <email": null>
2016-12-16 19:22:56 -0800

    added google maps tweet mapping functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,104 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function initialize() {
+
+
+  var locations = [
+  ['San Diego', 32, -117, 4]];
+
+  window.map = new google.maps.Map(document.getElementById('map'), {
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var infowindow = new google.maps.InfoWindow();
+
+  var bounds = new google.maps.LatLngBounds();
+
+  for (i = 0; i < locations.length; i++) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      map: map
+    });
+
+    bounds.extend(marker.position);
+
+    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+      return function () {
+        infowindow.setContent(locations[i][0]);
+        infowindow.open(map, marker);
+      }
+    })(marker, i));
+  }
+
+  map.fitBounds(bounds);
+
+  var listener = google.maps.event.addListener(map, "idle", function () {
+    map.setZoom(7);
+    google.maps.event.removeListener(listener);
+  });
+}
+
+
+  function addMarker(location) {
+        marker = new google.maps.Marker({
+            position: location,
+            map: map
+        });
+    }
+
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
+
+
+$(document).ready(function(){
+  $.ajax({
+    url: '/',
+    method: "GET",
+    dataType: "JSON"
+  }).done(function(response){
+    var tweetResponse = response;
+    console.log(tweetResponse);
+
+    // var latitude = tweetResponse[1].place.bounding_box.coordinates[0][1][0];
+    // var longitude = tweetResponse[1].place.bounding_box.coordinates[0][1][1];
+
+  //   for (i = 0; i < locations.length; i++) {
+  //   newMarker = new google.maps.Marker({
+  //     position: new google.maps.LatLng(locations[0][0], locations[0][1]),
+  //     map: map
+  //   });
+  //   newMarker.setMap(map)
+  // }
+
+    console.log(typeof(geoResultLong))
+      function TestMarker(lat, long) {
+       CentralPark = new google.maps.LatLng(lat, long);
+       addMarker(CentralPark);
+    }
+
+      tweetResponse.forEach(function(element, elementIndex) {
+        if(tweetResponse[elementIndex].place !== null){
+          var latitude = tweetResponse[elementIndex].place.bounding_box.coordinates[0][1][0];
+          var longitude = tweetResponse[elementIndex].place.bounding_box.coordinates[0][1][1];
+          TestMarker(longitude,latitude);
+        }else{
+          console.log('no coordinates for' + element)
+        }
+      });
+
+
+})
+
+})
+
+
+
