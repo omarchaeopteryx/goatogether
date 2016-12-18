f4ac016f2e52ec81b015f18001c73df85a01c074
Jack Shelley <email": null>
2016-12-18 13:37:49 -0800

    changed root from users to lcoations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   resource :user
-  root 'users#index'
+  resources :locations
+  root 'locations#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
