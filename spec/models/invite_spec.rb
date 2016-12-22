e80bb3ac45f36dad8f67e6e3133ebd0d91684387
omarchaeopteryx <omaralimalik@gmail.com>
2016-12-22 10:26:42 -0800

    Update Gemfile, add minor spec tests

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Invite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "making an invite object" do
+    
+    it "instantiates an invite object" do
+      invite = Invite.new
+      expect(invite).to be_an_instance_of(Invite)
+    end
+
+    it "has a guest associated with an invite" do
+      user = User.new(id:1)
+      invite = Invite.new(guest_id:1)
+      expect(invite.guest).to be_an_instance_of(User)
+    end
+  end
 end
