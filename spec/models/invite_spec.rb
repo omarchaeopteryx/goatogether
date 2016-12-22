1872a25477c53d39bae65b489aceb9d6a322f7e5
Omar <email": null>
2016-12-22 14:11:25 -0800

    Add configuration settings to precompile assets on heroku

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -2,16 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Invite, type: :model do
   context "making an invite object" do
-    
+
     it "instantiates an invite object" do
       invite = Invite.new
       expect(invite).to be_an_instance_of(Invite)
     end
 
-    it "has a guest associated with an invite" do
-      user = User.new(id:1)
-      invite = Invite.new(guest_id:1)
-      expect(invite.guest).to be_an_instance_of(User)
-    end
+
   end
 end
