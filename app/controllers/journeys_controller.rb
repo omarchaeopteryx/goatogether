f11bedd4b201d0806eb6f8d31e9bc3a4d02a982b
omarchaeopteryx <omaralimalik@gmail.com>
2016-12-23 11:01:26 -0800

    Fix bug for demo

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -56,6 +56,7 @@ class JourneysController < ApplicationController
       end
       if request.xhr? # use responders instead of xhr? method
         @upcoming_journeys = Journey.by(current_user).upcoming
+        @current_journeys = Journey.by(current_user).current
         @previous_journeys = Journey.by(current_user).previous
         @accepted_invitations = Invite.by(current_user).positive
         @pending_invitations = Invite.where("guest_id = ? AND response IS ?", current_user.id, nil).order("created_at DESC")
@@ -160,14 +161,14 @@ private
   end
 
 
-  def twitter_search(twitter_client, search_term, radius, max_id=nil, results=[], pins=2)
+  def twitter_search(twitter_client, search_term, radius, max_id=nil, results=[], pins=25)
     search_term = search_term.to_s
     if results.length >= pins
       results.slice!(pins..-1)
       return results
     else
       puts "#{radius.to_i}"
-      results2 =  current_user.twitter.search(search_term, geocode:"32,-117,#{radius}mi", max_id: max_id).take(5).to_a
+      results2 =  current_user.twitter.search(search_term, geocode:"32,-117,#{radius}mi", max_id: max_id).take(15).to_a
       results = results.concat(results2)
       max_id = results.last.id
 
