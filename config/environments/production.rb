8ff4a1fe64888674c215c172ad4853831727875d
Omar <email": null>
2016-12-22 17:42:51 -0800

    Add SSL to production environment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 Rails.application.configure do
 
   config.serve_static_assets = true
-  config.assets.precompile = true
 
   # Settings specified here will take precedence over those in config/application.rb.
   config.public_file_server.enabled = true
@@ -28,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -45,7 +44,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
