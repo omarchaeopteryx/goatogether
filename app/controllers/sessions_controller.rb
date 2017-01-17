71a3264b86dbd8ce2f09462b9413d11072229266
omarchaeopteryx <omaralimalik@gmail.com>
2017-01-16 21:24:55 -0800

    Remove counter due to server order load error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = @user.id
-    session[:count] = -1 # Add a counter to only run default search upon first visit. Note that we start at negative one because auth runs load twice.
     redirect_to '/'
   end
 
