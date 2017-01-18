9eeb3f472e0ef41ad5218abceedecbf1c0c90b04
omarchaeopteryx <omaralimalik@gmail.com>
2017-01-18 10:01:20 -0800

    Remove hard-coded lat long and set as default search_location input

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -34,19 +34,19 @@ class LocationsController < ApplicationController
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
- def twitter_search(twitter_client, search_term, max_id=nil, results=[], pins=1)
+ def twitter_search(twitter_client, search_term, search_location="32,-117,100mi", max_id=nil, results=[], pins=1)
     search_term = search_term.to_s
     if results.length >= pins
       results.slice!(pins..-1)
       return results
     else
 
-      results2 =  current_user.twitter.search(search_term, geocode:"32,-117,100mi", max_id: max_id).take(5).to_a
+      results2 =  current_user.twitter.search(search_term, geocode: search_location, max_id: max_id).take(5).to_a
       results = results.concat(results2)
       max_id = results.last.id
 
       results.select!{|tweet| tweet.geo? }
-      twitter_search(twitter_client,search_term, max_id, results)
+      twitter_search(twitter_client, search_term, search_location, max_id, results)
     end
   end
 
