8661e37b902b78260a0bcf273d48ee511458cf71
omarchaeopteryx <omaralimalik@gmail.com>
2017-05-21 12:14:23 -0700

    Update geocoder lookup specifications

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,1 +1,12 @@
-Geocoder.configure(ip_lookup: :telize)
+Geocoder.configure(
+  # geocoding service (see below for supported options):
+  :lookup => :nomatim,
+  # IP address geocoding service (see below for supported options):
+  :ip_lookup => :free_geoip,
+
+   # to use an API key:
+  # :api_key => "...",
+
+   # geocoding service request timeout, in seconds (default 3):
+   :timeout => 5
+)
