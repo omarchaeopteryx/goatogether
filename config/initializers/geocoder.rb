91cb7d6cac22a334267c34552a3632b73ea3255d
omarchaeopteryx <omaralimalik@gmail.com>
2017-05-22 10:45:53 -0700

    Change configs to add google

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,11 +2,11 @@ Geocoder.configure(
   # geocoding service (see below for supported options):
   # :lookup => :google,
   # # IP address geocoding service (see below for supported options):
-  # :ip_lookup => :google,
-  # :use_https => true,
+  :ip_lookup => :google,
+  :use_https => true,
   #
   #  # to use an API key:
-  # :api_key => ENV["GOOGLE_API_KEY"],
+  :api_key => ENV["GOOGLE_API_KEY"],
 
    # geocoding service request timeout, in seconds (default 3):
    :timeout => 10 # CHECK against avg time for API to return results.
